7f3f1f846a80e2f0040bb913a57668bbb7509014
Sk Wasim Rajjak <88873105+skwasimrajjak@users.noreply.github.com>
2024-08-26 20:51:49 +0000

    Contract Deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,17 @@
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
+require('dotenv').config();
+
+//ImageStorage deployed to: 0x375Fa8fcb3352d694E25110CEc2eEedfDEc7859e
+
+const { PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
     solidity: "0.8.21",
     networks: {
         opencampus: {
             url: `https://rpc.open-campus-codex.gelato.digital/`,
-            accounts: [PRIVATE_KEY],
+            accounts: [`0x${PRIVATE_KEY}`]
           },
     },
     etherscan: {
